e6f70f8c0b3355cff9d5344bad334ff5413f39fe
Krypie <krypie65@gmail.com>
2024-06-26 19:10:17 +0200

    added styles

--- src/dzien2_frontend/src/main.js
+++ src/dzien2_frontend/src/main.js
@@ -1,6 +1,7 @@
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import './index.scss';
+import '/home/xardi/work/intenetcomputer/dzien2/src/dzien2_frontend/src/components/index.css';
 import App from './App.vue';
 
 createApp(App).use(createPinia()).mount('#app');
